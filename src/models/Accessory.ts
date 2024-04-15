92c16c6cd8a4100a7f22ab66319f0fcb95dfd9a4
Miracle <miraclepatidehen@gmail.com>
2024-04-15 03:10:34 +0100

    added accessory model

---
+++ src/models/Accessory.ts
@@ -0,0 +1,24 @@
+import { Schema, Model, SchemaTypes, Types } from "mongoose";
+import Instrument, {InstrumentI} from "./Instrument";
+
+interface AccessoryI{
+    name: string
+    description: string
+    img: string
+    related_instruments: [InstrumentI]
+    brand: Types.ObjectId
+    price: number
+    stock: number
+}
+
+const AccessorySchema = new Schema<AccessoryI>({
+  name: { type: String, maxLength: 20, required: true },
+  description: { type: String, required: true },
+  img: { type: String, required: true },
+  related_instruments: [Instrument],
+  brand: { type: SchemaTypes.ObjectId, ref: "Brand" },
+  price: { type: Number, required: true, max: 1000 },
+  stock: { type: Number, required: true },
+});
+
+export default Model.create("Accessory", AccessorySchema);
