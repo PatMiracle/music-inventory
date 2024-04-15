5b5adccb3ec5d4feb3f113b3d233f718a69c9c28
Miracle <miraclepatidehen@gmail.com>
2024-04-15 02:54:26 +0100

    added instrument model

---
+++ src/models/Instrument.ts
@@ -0,0 +1,28 @@
+import { Schema, Model, SchemaTypes, Types } from "mongoose";
+
+interface InstrumentI{
+    name: string
+    description: string
+    img: string
+    category: Types.ObjectId
+    brand: Types.ObjectId
+    price: number
+    stock: number
+}
+
+const InstrumentSchema = new Schema<InstrumentI>({
+  name: { type: String, maxLength: 20, required: true },
+  description: { type: String, required: true },
+  img: { type: String, required: true },
+  category: {
+    type: SchemaTypes.ObjectId,
+    ref: "Category",
+    lowercase: true,
+    required: true,
+  },
+  brand: { type: SchemaTypes.ObjectId, ref: "Brand" },
+  price: { type: Number, required: true, max: 1000 },
+  stock: { type: Number, required: true },
+});
+
+export default Model.create("Instrument", InstrumentSchema);
