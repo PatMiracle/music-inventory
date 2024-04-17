534fb797623ddb0e12e7210509e453cff80089ec
Miracle <miraclepatidehen@gmail.com>
2024-04-17 03:03:51 +0100

    feat: set up middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,40 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-import express from 'express'
-import morgan from 'morgan'
+import express from "express";
+import morgan from "morgan";
+import errorHandler from "errorhandler";
+
+import connectDb from "./db/connect";
+
+import inventoryRoute from "./routes/inventory";
+import compression from "compression";
+
+// Populate DB
+// import populate from "./db/populate";
+// populate().catch((e) => console.log(e));
 
 dotenv.config({ path: ".env" });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const app = express();
 
-const app = express()
+app.use(morgan("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(morgan("dev"))
+app.use("/inventory", inventoryRoute);
 
-app.get("/", (req, res)=>{
-    res.status(200).send("Hello there!")
-})
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler());
+}
 
-app.listen(port, ()=> console.log(`server running at port ${port}!`))
+async function main() {
+  try {
+    await connectDb();
+    app.listen(port, () => console.log(`server running at port ${port}!`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
