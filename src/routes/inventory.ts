f9d15d2256545425cd4cd3ebe79d9f54e00cfd8e
Miracle <miraclepatidehen@gmail.com>
2024-04-19 03:01:52 +0100

    fix: changed patch to put

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -9,7 +9,7 @@ const router = Router();
 router.get("/brands", brandController.index);
 router.post("/brands", brandController.addBrand);
 router.get("/brands/:brandName", brandController.getBrand);
-router.patch("/brands/:brandName", brandController.editBrand);
+router.put("/brands/:brandName", brandController.editBrand);
 router.delete("/brands/:brandName", brandController.deleteBrand);
 
 // Categories
@@ -17,7 +17,7 @@ router.delete("/brands/:brandName", brandController.deleteBrand);
 router.get("/categories");
 router.post("/categories");
 router.get("/categories/:categoryName");
-router.patch("/categories/:categoryName");
+router.put("/categories/:categoryName");
 router.delete("/categories/:categoryName");
 
 // Instruments
@@ -25,7 +25,7 @@ router.delete("/categories/:categoryName");
 router.get("/instruments");
 router.post("/instruments");
 router.get("/instruments/:instrumentID");
-router.patch("/instruments/:instrumentID");
+router.put("/instruments/:instrumentID");
 router.delete("/instruments/:instrumentID");
 
 // Accessories
@@ -33,7 +33,7 @@ router.delete("/instruments/:instrumentID");
 router.get("/accessories");
 router.post("/accessories");
 router.get("/accessories/:accessID");
-router.patch("/accessories/:accessID");
+router.put("/accessories/:accessID");
 router.delete("/accessories/:accessID");
 
 export default router;
