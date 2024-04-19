b173e3164934bd0f6b3f965a52ae3b87f5d579bd
Miracle <miraclepatidehen@gmail.com>
2024-04-19 17:07:05 +0100

    feat: accessory controller complete

--- src/controllers/accessory.ts
+++ src/controllers/accessory.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import Accessory, { AccessoryI } from "../models/Accessory";
 import { Request, Response } from "express";
 
@@ -39,11 +40,92 @@ export const addAccessory = async (req: Request, res: Response) => {
   if (brand) accessoryDetail.brand = brand;
   if (features) accessoryDetail.features = features;
 
-  return res.status(201).json({ message: `Added instrument: ${name}` });
+  return res.status(201).json({ message: `Added accessory: ${name}` });
 };
 
-export const getAccessory = async (req: Request, res: Response) => {};
+export const getAccessory = async (req: Request, res: Response) => {
+  const { accessID } = req.params;
 
-export const editAccessory = async (req: Request, res: Response) => {};
+  if (!isValidObjectId(accessID)) {
+    return res.status(400).json({ message: `id ${accessID} is invalid` });
+  }
 
-export const deleteAccessory = async (req: Request, res: Response) => {};
+  const accessory = await Accessory.findById(accessID);
+
+  if (Object.keys(accessory).length) {
+    return res.status(200).json(accessory);
+  } else {
+    return res.status(404).json({
+      message: `accessory with id: ${accessID} does not exist`,
+    });
+  }
+};
+
+export const editAccessory = async (req: Request, res: Response) => {
+  const { accessID } = req.params;
+  const {
+    name,
+    description,
+    img,
+    related_instruments,
+    brand,
+    features,
+    price,
+    stock,
+  } = req.body;
+
+  if (!isValidObjectId(accessID)) {
+    return res.status(400).json({ message: `id ${accessID} is invalid` });
+  }
+
+  const accessoryExists = await Accessory.findById(accessID);
+
+  if (accessoryExists) {
+    const updateAccessory: {
+      name?: string;
+      description?: string;
+      img?: string;
+      related_instruments?: string;
+      brand?: string;
+      features?: string[];
+      price?: number;
+      stock?: number;
+    } = {};
+
+    if (name) updateAccessory.name = name;
+    if (description) updateAccessory.description = description;
+    if (img) updateAccessory.img = img;
+    if (related_instruments)
+      updateAccessory.related_instruments = related_instruments;
+    if (brand) updateAccessory.brand = brand;
+    if (features) updateAccessory.features = features;
+    if (price) updateAccessory.price = price;
+    if (stock || stock === 0) updateAccessory.stock = stock;
+
+    await Accessory.findByIdAndDelete(accessID, updateAccessory)
+      .then(() => {
+        res.status(201).send({ message: "update success" });
+      })
+      .catch((e) => res.status(500).send({ message: "update unsuccessful" }));
+  } else {
+    res.status(404).json({
+      message: `accessory with id: ${accessID} does not exist`,
+    });
+  }
+};
+
+export const deleteAccessory = async (req: Request, res: Response) => {
+  const { accessID } = req.params;
+
+  if (!isValidObjectId(accessID)) {
+    return res.status(400).json({ message: `id ${accessID} is invalid` });
+  }
+
+  await Accessory.findByIdAndDelete(accessID)
+    .then(() => {
+      res.status(200).send({ message: "delete success" });
+    })
+    .catch((e) => {
+      res.status(500).json({ message: "delete action failed" });
+    });
+};
